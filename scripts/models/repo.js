2b2da202aabfe8264f199b61c564bcdee2ba7dc6
Rick Patci <rpatci@gmail.com>
2016-06-22 11:47:04 -0700

    refactor blog application for heroku in order for the entire world to see!

--- scripts/models/repo.js
+++ scripts/models/repo.js
@@ -6,16 +6,11 @@
   // TODO: Refactor this ajax method into a get method to the proxy
   //  'end point' provided by server.js.
   repos.requestRepos = function(callback) {
-    $.ajax({
-      url: 'https://api.github.com/users/codefellows/repos' +
-            '?per_page=10' +
-            '&sort=updated',
-      type: 'GET',
-      headers: { 'Authorization': 'token ' + githubToken },
-      success: function(data, message, xhr) {
-        repos.all = data;
-      }
-    }).done(callback);
+    $.get('/github/users/codefellows-seattle-301d7/repos' +
+          '?per_page=10&sort=updated')
+          .done(function(data) {
+            repos.allRepos = data;
+          }).done(callback);
   };
 
   repos.withTheAttribute = function(attr) {
